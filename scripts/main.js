5987305b11be89e0f7ce4bbf81b9eccf293ec211
Aleks13YU <149099661+Aleks13YU@users.noreply.github.com>
2023-10-26 23:51:41 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+var myImage = document.querySelector("img");
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/new-project.png") {
+    myImage.setAttribute("src", "images/web-development.png");
+  } else {
+    myImage.setAttribute("src", "images/new-project.png");
+  }
+};
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+function setUserName() {
+  var myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "NEW PROJECT Юферев А.В., " + myName;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = "NEW PROJECT Юферев А.В., " + storedName;
+}
+myButton.onclick = function () {
+  setUserName();
+};
