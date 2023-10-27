cf7e79d057d071752c8772dff31bb05ea7fc0689
Aleks13YU <149099661+Aleks13YU@users.noreply.github.com>
2023-10-27 09:27:58 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,13 +12,13 @@ var myHeading = document.querySelector("h1");
 function setUserName() {
   var myName = prompt("Please enter your name.");
   localStorage.setItem("name", myName);
-  myHeading.textContent = "NEW PROJECT, " + myName;
+  myHeading.textContent = "ПРИВЕТ, " + myName;
 }
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   var storedName = localStorage.getItem("name");
-  myHeading.textContent = "NEW PROJECT, " + storedName;
+  myHeading.textContent = "ПРИВЕТ, " + storedName;
 }
 myButton.onclick = function () {
   setUserName();
